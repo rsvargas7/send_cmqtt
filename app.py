5eb7e7f1f9a8d54a4a492b0350567d08a25f7eae
rsvargas7 <rsvargas.742@gmail.com>
2025-04-03 10:21:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if st.button('ON'):
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("Daarkin blade", message)
+    ret= client1.publish("Daarkin", message)
  
     #client1.subscribe("Sensores")
     
@@ -52,7 +52,7 @@ if st.button('OFF'):
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("Daarkin blade", message)
+    ret= client1.publish("Daarkin", message)
   
     
 else:
